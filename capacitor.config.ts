35e873aec181f98585051bdadc59d8bc95896ba2
Zayn al-Abidin <sly86055r@gmail.com>
2026-07-04 16:44:53 +0300

    Fix Codemagic Prisma build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.sadeem.app',
   appName: 'Sadeem',
   webDir: 'out',
+  server: {
+    url: 'http://192.168.0.124:3000', // هذا راح يخلي التطبيق يتصل بسيرفر حاسبتك
+    cleartext: true // هاي ضرورية حتى يشتغل رابط http العادي بدون مشاكل
+  },
   plugins: {
     FirebaseAuthentication: {
       skipNativeAuth: false,
